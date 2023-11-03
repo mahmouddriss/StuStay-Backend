4fba48b86195d187884661b5d72b8427f5354ed8
mahmouddriss <mahmoud.driss@esprit.tn>
2023-11-03 20:10:07 +0100

    First Commit

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const upload = require('../middleware/upload')
+const upload = require('../middlewares/upload')
 const AuthController = require('../controllers/AuthController')
 
 
--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-import express from 'express';
+
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoute = require('./routes/AuthRoute');
+const userRoute = require('./routes/UserRoute');
+
+
 const app = express();
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 9090;
 
-//app.get('/' ,(req,res)=> {
-   // res.status(200).json({message:'hello world!'});
-//})
-
-import gameRoutes from './routes/game.js';
-app.use(express.json());
-app.use('/game',gameRoutes);
-
-app.listen (port,hostname,() => {
-
-    console.log(`Server running at http://localhost:${port}/`);
+// Connect to your MongoDB database
+mongoose.connect('mongodb+srv://mahmouddriss:fOL7IeCGeoO5eMal@cluster0.scij7rk.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+const db = mongoose.connection;
+
+
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+  // Your code here
+});
+
+// Configure Express to parse JSON
+app.use(bodyParser.json());
+app.use('/user', authRoute)
+app.use('/user', userRoute)
+
+app.use(express.static('public'));  
+app.use('/user/avatar', express.static('uploads/avatar'));
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT)
+})
+
+
+
+
 
